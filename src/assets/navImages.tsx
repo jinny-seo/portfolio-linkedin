3cc13e5da059afca10ffd7e5bd9b20e8d9db0fa3
Jinny Seo <jinny@Jinnys-iMac-10.attlocal.net>
2025-02-26 11:52:16 -0600

    Added nav images to src/assets

---
+++ src/assets/navImages.tsx
@@ -0,0 +1,19 @@
+import computer from './nav/1-computer.svg';
+import computerHover from './nav/1-computer-hover.svg';
+import art from './nav/2-art.svg';
+import artHover from './nav/2-art-hover.svg';
+import note from './nav/3-note.svg';
+import noteHover from './nav/3-note-hover.svg';
+import mail from './nav/4-mail.svg';
+import mailHover from './nav/4-mail-hover.svg';
+
+export const navAssets = {
+    computer,
+    computerHover,
+    art,
+    artHover,
+    note,
+    noteHover,
+    mail,
+    mailHover,
+};

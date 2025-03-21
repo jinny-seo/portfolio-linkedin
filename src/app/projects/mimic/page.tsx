05d0f37cec615b2f7ebc0797aa616b37ed6a6824
Jinny Seo <jinny@Mac.attlocal.net>
2025-03-21 11:46:43 -0500

    Updated Mimic project link

---
+++ src/app/projects/mimic/page.tsx
@@ -0,0 +1,9 @@
+export default function mimicProject() {
+    return (
+      <div className="p-6 max-w-4xl mx-auto">
+        <h1 className="text-3xl font-bold">Mimic</h1>
+      
+      </div>
+    );
+  }
+  

8b8cbc627845be346e03a750c1c613bdabedbff2
Jinny Seo <jinny@Jinnys-iMac-10.attlocal.net>
2025-03-22 23:33:59 -0500

    Updated right spacing for project items on the home page

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -20,22 +20,24 @@ interface ProjectItemProps {
 
 export const ProjectItem: React.FC<ProjectItemProps> = ({name, company, description, date, link, image, prevProjLink, nextProjLink}) => {
     return(
-        <div className="md:relative md:mr-[72px] lg:mr-[350px] xl:mr-[500px] 2xl:mr-[600px] lg:max-w-[1024px]">
-            <Windows95FrameOuter label={name}> 
-                <Windows95FrameInner>
-                    <Image src={image} alt="test" className=""/>
-                </Windows95FrameInner>
+        <div className="md:relative md:mr-[72px] lg:mr-[350px] xl:mr-[400px] lg:max-w-[1200px]">
+            <Windows95FrameOuter label={name}>
+                <a href={link}>
+                    <Windows95FrameInner>
+                        <Image src={image} alt="test" className=""/>
+                    </Windows95FrameInner>
+                </a>
                 {/* Show description and nav buttons when screen size is small: START */}
-                <div className='flex flex-col mx-1 mb-4 mt-1 md:hidden'>
+                <div className='flex flex-col gap-1 mx-1 mb-4 mt-1 md:hidden'>
                     <p className=" px-[0.85rem] py-[0.6rem] max-w-none mb-0">{description}</p>
                     <ProjectPreviewNav currProjLink={link} prevProjLink={prevProjLink} nextProjLink={nextProjLink} />
                 </div>
                 {/* Show description and nav buttons when screen size is small: END */}
             </Windows95FrameOuter>
             <div className="hidden md:block">
-                <div className="absolute top-1/2 -right-[72px] w-[380px] lg:-right-[56px] xl:-right-[168px] lg:w-[400px] xl:w-[480px]">
+                <div className="absolute top-1/2 -right-[72px] w-[380px] lg:-right-[56px] xl:-right-[132px] lg:w-[400px] xl:w-[480px]">
                     <Windows95FrameOuter label={company} date={date}>
-                        <div className="pb-4">
+                        <div className="flex flex-col gap-1 pb-4">
                             <p className=" px-[0.85rem] py-[0.6rem] max-w-none mb-0">{description}</p>
                             <ProjectPreviewNav currProjLink={link} prevProjLink={prevProjLink} nextProjLink={nextProjLink} />
                         </div>

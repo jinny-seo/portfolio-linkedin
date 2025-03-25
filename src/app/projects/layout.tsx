ca3811197b9919f2621f46ce34eeab1f0e0d2c08
Jinny Seo <jinny@Jinnys-iMac-10.attlocal.net>
2025-03-25 16:28:10 -0500

    Making sure everything is updated

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -8,7 +8,7 @@ import CaseStudyNav from "@/components/CaseStudyNav";
 export default function ProjectsLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-hero-grid">
-    <div className="mx-auto max-w-[1100px] px-container ">
+    <div className="mx-auto max-w-[1100px] p-container ">
       
       <Windows95FrameOuter>
         <main className="flex flex-col ">

0467638c237bbf21afe381898bc13a55a01381c0
Jinny Seo <jinny@Jinnys-iMac-10.attlocal.net>
2025-04-10 16:43:09 -0500

    Updated open-next.config.mjs

--- .open-next/env/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"NEXT_PUBLIC_MIXPANEL_TOKEN":"1537fb5330fabd26324fc34a650eda77"};
-export const development = {"NEXT_PUBLIC_MIXPANEL_TOKEN":"1537fb5330fabd26324fc34a650eda77"};
-export const test = {};
